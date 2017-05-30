accb8ac79ad9ee776b8b762669a3a77c1e3404aa
Chelong <chelong821204@gmail.com>
2017-05-30 23:56:01 +0800

    modified send message view use cable.js click, cable connection use current_user verify, modify actioncable font-end and back-end

--- app/assets/javascripts/channels/conversation.js
+++ app/assets/javascripts/channels/conversation.js
@@ -1,17 +1,18 @@
 App.conversation = App.cable.subscriptions.create("ConversationChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
-
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
-
+  connected: function() {},
+  disconnected: function() {},
   received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
+    console.log(data['message']);
   },
-
-  speak: function() {
-    return this.perform('speak');
+  speak: function(message) {
+    return this.perform('speak', {
+      message: message
+    });
   }
+});
+$(document).on('submit', '.new_message', function(e) {
+  e.preventDefault();
+  var values = $(this).serializeArray();
+  App.conversation.speak(values);
+  $(this).trigger('reset');
 });
