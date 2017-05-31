105a4a70d5a605bfa69403e282a22d53bc237d90
Chelong <chelong821204@gmail.com>
2017-05-31 18:08:18 +0800

    modified MessageBroadcastJob recipient method, add window HTML partial is full conversation window

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -22,6 +22,7 @@ class MessageBroadcastJob < ApplicationJob
   def broadcast_to_recipient(user, message)
     ActionCable.server.broadcast(
       "conversations-#{user.id}",
+      window: render_window(message.conversation, user),
       message: render_message(message, user),
       conversation_id: message.conversation_id
     )
