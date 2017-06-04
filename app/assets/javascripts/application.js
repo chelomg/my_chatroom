aa709e07e5596bd57a5b6eeaff3dd51ab26180b5
Chelong <chelong821204@gmail.com>
2017-06-04 19:52:07 +0800

    feature open minilize window if view > 2, other window will hide

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,17 +17,22 @@
 
 (function() {
   $(document).on('click', '.toggle-window', function(e) {
-      e.preventDefault();
-      var panel = $(this).parent().parent();
-      var messages_list = panel.find('.messages-list');
+    e.preventDefault();
+    var panel = $(this).parent().parent();
+    var messages_list = panel.find('.messages-list');
 
-      panel.find('.panel-body').toggle();
-      panel.attr('class', 'panel panel-default');
+    if ($('#conversations-list').children().length > 2) {
+      $('#conversations-list li').children().removeClass('panel-default').addClass('panel-warning');
+      $('#conversations-list li div .panel-body').hide();
+    }
 
-      if (panel.find('.panel-body').is(':visible')) {
-            var height = messages_list[0].scrollHeight;
-            messages_list.scrollTop(height);
-          }
-    });
+    panel.find('.panel-body').toggle();
+    panel.attr('class', 'panel panel-default');
+
+    if (panel.find('.panel-body').is(':visible')) {
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+  });
 })();
 
