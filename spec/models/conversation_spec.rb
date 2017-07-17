64279167c07b6da1fd7e53a953d32c591f2a5af9
Chelong <chelong821204@gmail.com>
2017-07-18 00:20:36 +0800

    add associate relation rspec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Conversation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:messages) }
+  it { should belong_to(:sender) }
+  it { should belong_to(:recipient) }
 end
