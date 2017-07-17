a4c030120f53a34a63db88d12838c47fbb217ef3
Chelong <chelong821204@gmail.com>
2017-07-18 00:45:00 +0800

    add validate scope rspec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Conversation, type: :model do
   it { should have_many(:messages) }
   it { should belong_to(:sender) }
   it { should belong_to(:recipient) }
+  it { should validate_uniqueness_of(:sender_id).scoped_to(:recipient_id) }
 end
