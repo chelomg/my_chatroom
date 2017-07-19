bf8d75f4dd14417e16fac5e09272a73ab14511b7
Chelong <chelong821204@gmail.com>
2017-07-19 23:40:51 +0800

    list conversation spec todo

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Conversation, type: :model do
   it { should belong_to(:sender) }
   it { should belong_to(:recipient) }
   it { should validate_uniqueness_of(:sender_id).scoped_to(:recipient_id) }
+  it '.between' do
+  end
+
+  it '.my_relation' do
+  end
+
+  it '.get' do
+  end
+
+  it '#opposed_user' do
+  end
+
+  it '.find_request_users' do
+  end
+
+  it '.find_friends' do
+  end
 end
