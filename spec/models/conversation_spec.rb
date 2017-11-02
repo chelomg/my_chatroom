5a555bb3e2f6fec774e009ad34da029f75013621
Chelong <chelong821204@gmail.com>
2017-11-02 10:25:42 +0800

    add #oppesed spec

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -41,6 +41,11 @@ RSpec.describe Conversation, type: :model do
   end
 
   it '#opposed_user' do
+    user1 = create(:user)
+    user2 = create(:user)
+    conversation = create(:conversation, recipient_id: user1.id, sender_id: user2.id)
+    expect(conversation.opposed_user(user1)).to eq(user2)
+    expect(conversation.opposed_user(user2)).to eq(user1)
   end
 
   it '.find_request_users' do
